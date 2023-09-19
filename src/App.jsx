3e97e10aa580f730f288a8fa980267bdf76c8eec
Rjchi <richardospina18@gmail.com>
2023-09-19 16:31:00 -0500

    [Refactor] Agrega el componente Login y genera una ruta para este componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import { PedidoContextProvider } from "./context/PedidoCotext";
 
 import Cocina from "./pages/Cocina";
 import { Caja } from "./pages/Caja";
-import { NotFound } from "./pages/NotFound";
-
-import { PedidoContextProvider } from "./context/PedidoCotext";
 import { Menu } from "./pages/Menu";
+import { Login } from "./pages/Login";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
     <div className="container mx-auto my-auto p-4 h-auto w-full flex flex-col items-center justify-center">
       <PedidoContextProvider>
         <Routes>
-          <Route path="/" element={<Menu />} />
+          <Route path="/" element={<Login />} />
+
+          <Route path="/menu" element={<Menu />} />
 
           <Route path="/cocina" element={<Cocina />} />
           <Route path="/caja" element={<Caja />} />
