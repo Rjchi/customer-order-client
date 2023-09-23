dde3c7000a848c33fb42bac7910ca2bcb631b6d9
BrayanGN <brayan6omez@gmail.com>
2023-09-23 10:26:12 -0500

    creacion de archivo para inicioSecion.api

---
+++ src/api/inicioSecion.api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = import.meta.env.VITE_API_URL;
+
+export const logueoRequest = async (user) =>
+  await axios.post(`${API}/api/logueo`, user);
+
+export const registroRequest = async (user) =>
+  await axios.post(`${API}/api/registro`,user );
+
