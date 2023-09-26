16309d424c282def2457a0ade9edb1900f3b5ec1
BrayanGN <brayan6omez@gmail.com>
2023-09-26 14:18:35 -0500

    Cambio

--- src/components/Navegation/NavBar.jsx
+++ src/components/Navegation/NavBar.jsx
@@ -6,21 +6,38 @@ const NavBar = () => {
     // <div className="w-full fixed top-0 left-0 z-30 lg:grid lg:grid-cols-10 ">
     <div className="w-full fixed top-0 left-0 z-30">
       <div className="col-span-8 col-start-2">
-        <nav className="flex justify-center bg-slate-500 bg-opacity-80 border border-black shadow-lg backdrop-blur-md font-mono font-bold ">
+        <nav className="flex justify-center bg-black bg-opacity-80 border border-blue-950 shadow-lg backdrop-blur-md font-mono font-bold ">
           <div className="w-full flex items-center justify-between mt-0 px-6 py-2">
+            <label
+              htmlFor="menu-toggle"
+              className="cursor-pointer md:hidden block"
+            >
+              <svg
+                className="fill-current text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+              >
+                <title>menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+              </svg>
+            </label>
+            <input className="hidden" type="checkbox" id="menu-toggle" />
+
             <div
               className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1"
               id="menu"
             >
               <nav>
-                <ul className="flex items-center justify-between space-x-3 text-base text-white hover:text-gray-400 pt-4 md:pt-0">
-                  <li className="hover:border-b hover:border-stone-800 ease-out duration-300 hover:border-b-4 hover:text-white">
+                <ul className="flex items-center justify-between space-x-3 text-base text-blue-600 pt-4 md:pt-0">
+                  <li className="hover:text-gray-50">
                     <Link to={`/cocina`}>Cocina</Link>
                   </li>
-                  <li className="hover:shadow-xl hover:border-b hover:border-stone-800 ease-out duration-300 hover:border-b-4 hover:text-white" >
+                  <li className="hover:text-gray-50">
                     <Link to={`/menu`}>Cliente</Link>
                   </li>
-                  <li className="hover:shadow-xl hover:border-b hover:border-stone-800 ease-out duration-300 hover:border-b-4 hover:text-white">
+                  <li className="hover:text-gray-50">
                     <Link to={`/caja`}>Caja</Link>
                   </li>
                 </ul>
