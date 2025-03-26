0d509fd0839f1a8b4a0f45872f65b3c99558d9e6
Rjchi <richardospina18@gmail.com>
2025-03-25 21:09:02 -0500

    [Docs] Modificar configuración de la aplicación

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+const api = import.meta.env.VITE_API_URL;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `${api}`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
+    host: '192.168.100.80',
+    // host: 'localhost',
+    port: 4173
+  },
+  build: {
+    outDir: './dist'
+  }
 })
