d81ad58dc648f73869027f7c74243402e1f0d483
Rjchi <richardospina18@gmail.com>
2025-03-26 21:46:59 -0500

    [Docs] Modificar configuracion del proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://192.168.100.80:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-    host: '192.168.100.80',
-    // host: "localhost",
+    host: '0.0.0.0',
     port: 4173,
   },
   build: {
